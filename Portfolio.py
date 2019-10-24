63bcb2e1fe7dfe0149bde13182ea0daabfae7008
tascoma <56949246+tascoma@users.noreply.github.com>
2019-10-23 23:43:47 -0500

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -1,1 +1,20 @@
+import openpyxl
+
+wb = openpyxl.load_workbook('Portfolio Management.xlsx')
+sheetACP = wb['Adj Close Prices']
+sheetReturns = wb['Returns']
+columns = ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
+
+
+for col in columns:
+    x = 6
+    cell1 = sheetACP[col + str(x)].value
+    while cell1:
+        cell2 = sheetACP[col + str(x - 1)].value 
+        sheetReturns[col + str(x - 1)] = cell1/cell2 - 1
+        x += 1
+        cell1 = sheetACP[col + str(x)].value
+
+
+wb.save('Portfolio Management.xlsx')
 
